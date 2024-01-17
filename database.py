bda77ec081336384a025aa5dfff8a47fa829f4cc
annan-dejene <70013311+annan-dejene@users.noreply.github.com>
2024-01-17 22:16:49 +0000

    Added dynamic pages for each page

--- database.py
+++ database.py
@@ -21,3 +21,17 @@ def load_jobs_from_db():
   
   return jobs
 
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * from jobs WHERE id = :val"), 
+      {"val": id}
+    )
+
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0]._mapping)
+  
